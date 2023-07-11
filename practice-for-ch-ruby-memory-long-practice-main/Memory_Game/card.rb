f967586aca25de6b3f958177bf29121195f927ef
AJU <aulcena@gmail.com>
2023-07-11 14:36:57 -0400

    create pairs method

--- practice-for-ch-ruby-memory-long-practice-main/Memory_Game/card.rb
+++ practice-for-ch-ruby-memory-long-practice-main/Memory_Game/card.rb
@@ -1,8 +1,8 @@
 class Card 
     attr_reader :face_up
 
-    def initialize(val)
-        @card = val
+    def initialize
+        @card = ("a".."z").to_a.sample
         @face_up = false  # card is faced down
     end 
 
@@ -10,15 +10,33 @@ class Card
         @face_up = false  # turn the card back down 
     end
 
-    
+    def reveal?
+
+        puts @card
+
+        @face_up = true
+        
+    end
+
+    def pairs
+
+       
+
+    end
 
 
 
+# genterate pairs of the same val 
+# this can be a factory method 
+# val can relpy on alphabet sample 
+#alphabet can be a constant
 
 
 
-
-
+    # we need face value
+    # face up or down
+    # var track 
+    # method to display 
     #we need a var to track if card is up or down 
     # hide will turn the card over 
     # reveal will show the card
